7f918fffe3cb24f5535af2112f651ca0470b1791
Brian Zelip <bzelip@protonmail.com>
2021-06-24 06:58:32 -0400

    Export themes via index.js, add comment to workflow

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// silence is golden
+const darkDefault = require('./dist/dark-default.json');
+const darkDimmed = require('./dist/dark-dimmed.json');
+const dark = require('./dist/dark.json');
+const lightDefault = require('./dist/light-default.json');
+const light = require('./dist/light.json');
+
+module.exports = {
+  darkDefault,
+  darkDimmed,
+  dark,
+  lightDefault,
+  light
+};
