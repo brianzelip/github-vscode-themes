e6acd2df9ea49b2a630285c692c0d7022e44481b
Brian Zelip <bzelip@protonmail.com>
2022-01-30 05:54:10 -0500

    Export colorblind and lightHighContrast themes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const dark = require('./dist/dark.json');
+const darkColorblind = require('./dist/dark-colorblind.json');
 const darkDefault = require('./dist/dark-default.json');
 const darkDimmed = require('./dist/dark-dimmed.json');
 const darkHighContrast = require('./dist/dark-high-contrast.json');
-const dark = require('./dist/dark.json');
-const lightDefault = require('./dist/light-default.json');
 const light = require('./dist/light.json');
+const lightColorblind = require('./dist/light-colorblind.json');
+const lightDefault = require('./dist/light-default.json');
+const lightHighContrast = require('./dist/light-high-contrast.json');
+
 
 module.exports = {
+  dark,
+  darkColorblind,
   darkDefault,
   darkDimmed,
   darkHighContrast,
-  dark,
+  light,
+  lightColorblind,
   lightDefault,
-  light
+  lightHighContrast
 };
